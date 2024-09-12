abe0ba515c204685d90197c91cee64964382a1ae
rajeshphy <bittuiitk@gmail.com>
2024-09-12 14:57:50 +0530

    .

--- _posts/2024-09-12-Dirac-Oscillator.markdown
+++ _posts/2024-09-12-Dirac-Oscillator.markdown
@@ -4,8 +4,7 @@ date:   2024-09-12 00:00:35 +0530
 categories: research
 ---
 
-Dirac's free particle equation was developed to linearly express the relativistic relation between energy and momentum. The Dirac oscillator, a Dirac equation linear in both momentum and coordinates, which reduces to a harmonic oscillator with strong spin-orbit coupling in the non-relativistic limit.
-
+The Dirac oscillator is a relativistic quantum system that extends the Dirac equation by incorporating a linear dependence on both momentum and spatial coordinates. Originally conceived as a modification of the free particle Dirac equation, the Dirac oscillator offers a framework where the dynamics exhibit features of a harmonic oscillator with a significant spin-orbit coupling in the non-relativistic limit. This unique coupling results in a distinct energy spectrum and degeneracy structure, which sets it apart from the conventional quantum harmonic oscillator. The Dirac oscillator has attracted significant interest due to its applications in various areas of quantum mechanics, quantum field theory, and its role in exploring relativistic quantum systems with potential applications in particle physics and condensed matter.
 
 ---
 <div class="mermaid">
--- _site/assets/javascripts/lunr/lunr.store.js
+++ _site/assets/javascripts/lunr/lunr.store.js
@@ -25,7 +25,7 @@ var store = [{
         "url": "http://localhost:4000/research/research/2024/09/02/Yang-Mill-Theory.html"
       },{
         "title": "Dirac Oscillator",
-        "excerpt":"Dirac’s free particle equation was developed to linearly express the relativistic relation between energy and momentum. The Dirac oscillator, a Dirac equation linear in both momentum and coordinates, which reduces to a harmonic oscillator with strong spin-orbit coupling in the non-relativistic limit. graph TD;    A[1989-Dirac Oscillator-moshinsky]    click A \"/research/PAPERS-ORG/Dirac/1989-moshinsky-Dirac-Oscillator.pdf\";","categories": ["research"],
+        "excerpt":"The Dirac oscillator is a relativistic quantum system that extends the Dirac equation by incorporating a linear dependence on both momentum and spatial coordinates. Originally conceived as a modification of the free particle Dirac equation, the Dirac oscillator offers a framework where the dynamics exhibit features of a harmonic oscillator with a significant spin-orbit coupling in the non-relativistic limit. This unique coupling results in a distinct energy spectrum and degeneracy structure, which sets it apart from the conventional quantum harmonic oscillator. The Dirac oscillator has attracted significant interest due to its applications in various areas of quantum mechanics, quantum field theory, and its role in exploring relativistic quantum systems with potential applications in particle physics and condensed matter. graph TD;    A[1989-Dirac Oscillator-moshinsky]    click A \"/research/PAPERS-ORG/Dirac/1989-moshinsky-Dirac-Oscillator.pdf\";","categories": ["research"],
         "tags": [],
         "url": "http://localhost:4000/research/research/2024/09/12/Dirac-Oscillator.html"
       }]
